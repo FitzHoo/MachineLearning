593409513ef317c5a7e00bd0fe521c9292acb3e9
Fitz Hoo <huliangyong163@163.com>
2017-12-04 18:49:04 +0800

    summary of machine learning algorithms

---
+++ scikit-learn.py
@@ -0,0 +1,210 @@
+from sklearn.datasets import load_iris
+import numpy as np
+import pandas as pd
+
+
+# 数据提取
+iris = load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+# 数据划分
+# from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+# 特征缩放
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+# 模型训练
+from sklearn.linear_model import Perceptron
+ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0)
+ppn.fit(X_train_std, y_train)
+
+y_pred = ppn.predict(X_test_std)
+print("Misclassified samples: {}".format((y_test != y_pred).sum()))
+
+
+from sklearn.metrics import accuracy_score
+print('Accuracy: {}'.format(accuracy_score(y_test, y_pred)))
+
+
+# 图像展示
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.2):
+
+    # Setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot all samples
+    X_test, y_test = X[test_idx, :], y[test_idx]
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y ==cl , 1], alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
+
+    # highlight test samples
+    if test_idx:
+        X_test, y_test = X[test_idx, :], y[test_idx]
+        plt.scatter(X_test[:, 0], X_test[:, 1], c='b', alpha=0.1, linewidths=1,
+                    marker='o', s=55, label='test set')
+
+    plt.xlabel('petal length [Standardized]')
+    plt.ylabel('petal width [Standardized]')
+    plt.legend(loc='upper left')
+
+    return plt.show()
+
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X=X_combined_std, y=y_combined, classifier=ppn,
+                      test_idx=range(105, 150))
+
+
+# Logistic Regression
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression(C=1000.0, random_state=0)
+lr.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=lr,
+                      test_idx=range(105, 150))
+plt.xlabel('petal length [Standardized]')
+plt.ylabel('petal width [Standardized]')
+plt.legend(loc='upper left')
+plt.show()
+
+lr.predict_proba(X_test_std[0, :].reshape(1, -1))
+
+
+# L2 regularization or L2 shrinkage or weight decay
+def plot_regular_weights(X_train_std, y_train):
+    weights, params = [], []
+    for c in np.arange(-5.0, 5.0):
+        lr = LogisticRegression(C=10**c, random_state=0)
+        lr.fit(X_train_std, y_train)
+        weights.append(lr.coef_[1])
+        params.append(10 ** c)
+    weights = np.array(weights)
+    plt.plot(params, weights[:, 0], label='petal length')
+    plt.plot(params, weights[:, 1], ls='--', label='petal width')
+    plt.ylabel('weight coefficient')
+    plt.xlabel('C')
+    plt.legend(loc='upper left')
+    plt.xscale('log')
+    plt.show()
+    return
+
+
+# SVM
+from sklearn.svm import SVC
+svm = SVC(kernel='linear', C=1.0, random_state=0)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
+plt.xlabel('petal length [Standardized]')
+plt.ylabel('petal width [Standardized]')
+plt.legend(loc='upper left')
+plt.show()
+
+
+# logical xor  逻辑异或
+np.random.seed(0)
+X_xor = np.random.randn(200, 2)
+Y_xor = np.logical_xor(X_xor[:, 0] > 0, X_xor[:, 1] > 0)
+Y_xor = np.where(Y_xor, 1, -1)
+
+plt.scatter(X_xor[Y_xor == 1, 0], X_xor[Y_xor == 1, 1], c='b', marker='x', label='1')
+plt.scatter(X_xor[Y_xor == -1, 0], X_xor[Y_xor == -1, 1], c='r', marker='s', label='-1')
+plt.ylim(-3.0)
+plt.legend()
+plt.show()
+
+# Radial Basis Function Kernal (Guassian Kernel)
+svm_k = SVC(kernel='rbf', random_state=0, gamma=0.1, C=10.0)
+svm_k.fit(X_xor, Y_xor)
+plot_decision_regions(X_xor, Y_xor, classifier=svm_k)
+plt.legend(loc='upper left')
+plt.show()
+
+
+# 不纯度衡量标准.
+def gini(p):
+    return p * (1-p) + (1-p) * (p)
+
+def entropy(p):
+    return -p * np.log2(p) - (1 - p) * np.log2(1-p)
+
+def error(p):
+    return 1 - np.max([p, 1-p])
+
+x = np.arange(0.0, 1.0, 0.01)
+ent = [entropy(p) if p != 0 else None for p in x]
+sc_ent = [e * 0.5 if e else None for e in ent]
+err = [error(i) for i in x]
+fig = plt.figure()
+ax = plt.subplot(111)
+for i, lab, ls, c in zip([ent, sc_ent, gini(x), err],
+                          ['Entropy', 'Entropy(Scaled)', 'Gini Impurity', 'Misclassification Error'],
+                          ['-', '-.', '--', '-'],
+                          ['black', 'red', 'green', 'cyan']):
+    line = ax.plot(x, i, label=lab, ls=ls, color=c)
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=4, fancybox=True, shadow=False)
+ax.axhline(y=0.5, lw=1, c='k', ls='--')
+ax.axhline(y=1.0, lw=1, c='k', ls='--')
+plt.ylim([0.0, 1.1])
+plt.xlabel('p(i=1)')
+plt.ylabel('Impurity Index')
+plt.show()
+
+
+# Decision Tree
+from sklearn.tree import DecisionTreeClassifier
+tree = DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=0)
+tree.fit(X_train, y_train)
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X_combined, y_combined, classifier=tree, test_idx=range(105, 150))
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.show()
+
+
+# Random Forest
+from sklearn.ensemble import RandomForestClassifier
+forest = RandomForestClassifier(criterion='entropy', n_estimators=10, random_state=1, n_jobs=2)
+forest.fit(X_train, y_train)
+plot_decision_regions(X_combined, y_combined, classifier=forest, test_idx=range(105, 150))
+plt.xlabel('petal length')
+plt.ylabel('petal width')
+plt.legend(loc='upper left')
+plt.show()
+
+
+# KNN (k-nearest neighbor classifier)
+# instance-base learning
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=5, p=2, metric='minkowski')
+knn.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=knn, test_idx=range(105, 150))
+plt.xlabel('petal length [Standadized]')
+plt.ylabel('petal width [Standardized]')
+plt.show()
