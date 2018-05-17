456a27807a029e28d0341c6494bde93178db1c6a
Fitz Hoo <huliangyong163@163.com>
2018-05-17 10:05:38 +0800

    add annotation

--- AdaptiveLinearNeuron.py
+++ AdaptiveLinearNeuron.py
@@ -3,7 +3,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from sklearn.datasets import load_iris
+from matplotlib.colors import ListedColormap
 
 class AdaLinNeuGD(object):
     def __init__(self, eta=0.01, n_iter=50):
@@ -37,6 +38,29 @@ class AdaLinNeuGD(object):
 
     def predict(self, X):
         return np.where(self.activation(X) >= 0.0, 1, 0)
+
+
+def plot_decision_region(X, y, classifier, resolution=0.02):
+    # Set up marker generator and color map
+    markers = ['s', 'x', 'o', '^', 'v']
+    colors = ['red', 'blue', 'lightgreen', 'gray', 'cyan']
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # Plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # Plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl)
 
 
 def plot_adjust_parameters_curve(X, y):
@@ -92,14 +116,23 @@ class AdalineSGD(object):
         return self
 
     def _shuffle(self, X, y):
+        '''
+        Shuffle training data
+        '''
         r = np.random.permutation(len(y))
         return X[r], y[r]
 
     def _initialize_weights(self, m):
+        '''
+        Initialize weights to zeros
+        '''
         self.w_ = np.zeros(1 + m)
         self.w_initialized = True
 
     def _update_weights(self, xi, target):
+        '''
+        Apply Adaline learning rule to update the weights
+        '''
         output = self.net_input(xi)
         error = target - output
         self.w_[1:] += self.eta * xi.dot(error)
@@ -117,4 +150,64 @@ class AdalineSGD(object):
         return np.where(self.activation(X) >= 0.0, 1, 0)
 
 
+data = load_iris()
+df = pd.DataFrame(np.c_[data['data'], data['target']])
+y = df.iloc[0:100, 4].values
+X = df.iloc[:100, [0, 2]].values
+plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
+plt.xlabel('petal length')
+plt.ylabel('sepal length')
+plt.legend(loc='upper left')
+plt.show()
 
+eta1, eta2 = 0.01, 0.0001
+ada1 = AdaLinNeuGD(n_iter=10, eta=eta1).fit(X, y)
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+ax[0].plot(range(1, len(ada1.cost_)+1), np.log10(ada1.cost_), marker='o') 
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error)')
+ax[0].set_title('Adaline-Learning rate {}'.format(eta1))
+
+ada2 = AdaLinNeuGD(n_iter=10, eta=eta2).fit(X, y)
+ax[1].plot(range(1, len(ada1.cost_)+1), ada2.cost_, marker='o') 
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Sum-squared-error')
+ax[1].set_title('Adaline-Learning rate {}'.format(eta2))
+
+plt.show()
+
+# Feature Scaling
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+ada = AdaLinNeuGD(n_iter=15, eta=0.01)
+ada.fit(X_std, y)
+
+plot_decision_region(X_std, y, classifier=ada)
+plt.title('Adaline - Gradient Descent`')
+plt.xlabel('sepal length [Standardized]')
+plt.ylabel('petal length [Standardized]')
+plt.legend(loc='upper left')
+plt.show()
+
+plt.plot(range(1, len(ada.cost_)+1), ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Sum-squared-error')
+plt.show()
+
+sgd_ada = AdalineSGD(n_iter=15, eta=0.01, random_state=1)
+sgd_ada.fit(X_std, y)
+plot_decision_region(X_std, y, classifier=sgd_ada)
+plt.title('Adaline - Stochastic Gradient Descent')
+plt.xlabel('Sepal length [standardized]')
+plt.ylabel('petal length [Standardized]')
+plt.legend(loc='upper left')
+plt.show()
+
+plt.plot(range(1, len(sgd_ada.cost_)+1), sgd_ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Average Cost')
+plt.show()
